ff706ec0e5f064fc679ad727254521569d470238
KRVPerera <rukshan.viduranga@gmail.com>
2021-10-02 02:55:00 +0530

    initial commit

---
+++ cMain.h
@@ -0,0 +1,24 @@
+//
+// Created by Rukshan Perera on 2021-10-02.
+//
+
+#ifndef WATCHNEXT_CMAIN_H
+#define WATCHNEXT_CMAIN_H
+
+#include "wx/wxprec.h"
+
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+class cMain : public wxFrame {
+
+public:
+    cMain();
+    ~cMain();
+
+};
+
+
+#endif //WATCHNEXT_CMAIN_H
